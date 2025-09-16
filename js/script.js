a165981b52e2e4a20b0793fb4f2585fb5d6108c8
Miles Damian <darcmephisto@gmail.com>
2025-09-16 22:09:16 +0800

    Header is responsive

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("sideNav").style.width = "200px";
+    document.getElementById("sideNav").style.height = "900px";
+  }
+  
+  function closeNav() {
+    document.getElementById("sideNav").style.width = "0";
+    document.getElementById("sideNav").style.height = "0";
+  }
+  
