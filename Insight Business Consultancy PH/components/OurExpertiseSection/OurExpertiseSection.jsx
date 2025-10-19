d52ed1f956783dd854d53c1516480edadcc60ed9
Edwin Bañago <edwinbanago692@gmail.com>
2025-10-19 11:21:52 +0800

    Added routing to Expertise cards.

--- Insight Business Consultancy PH/components/OurExpertiseSection/OurExpertiseSection.jsx
+++ Insight Business Consultancy PH/components/OurExpertiseSection/OurExpertiseSection.jsx
@@ -12,62 +12,74 @@ import {
 
 import { FaPersonWalkingLuggage, FaFlag } from "react-icons/fa6";
 import { PiCertificateFill } from "react-icons/pi";
+import {Link} from "react-router-dom";
 
 const services = [
 	{
 		icon: <FaClipboardCheck className="text-4xl text-black"/>,
 		title: "Business Registration",
 		desc: "Hassle-free processing with SEC, DTI, BIR, LGU, and government agencies.",
+		src: 'business-registration'
 	},
 	{
 		icon: <FaCalculator className="text-4xl text-black"/>,
 		title: "Accounting & Bookkeeping",
 		desc: "Transparent financial reporting and systems for smarter decisions.",
+		src: 'bookkeeping-and-accounting'
 	},
 	{
 		icon: <FaFileInvoiceDollar className="text-4xl text-black"/>,
 		title: "Tax & Compliance",
 		desc: "Timely BIR, SEC, and government filings without stress.",
+		src: 'tax-and-regulatory-compliance'
 	},
 	{
 		icon: <FaUserClock className="text-4xl text-black"/>,
 		title: "Payroll Outsourcing",
 		desc: "Efficient payroll, attendance tracking, and compliance.",
+		src: 'payroll-outsourced'
 	},
 	{
 		icon: <FaFileSignature className="text-4xl text-black"/>,
 		title: "BIR One-Time Transactions",
 		desc: "Specialized support in sale, donation, and estate processing.",
+		src: 'bir-transactions'
 	},
 	{
 		icon: <FaRegBuilding className="text-4xl text-black"/>,
 		title: "Specialized Registrations",
 		desc: "SEC Amendments, AMLC, DOLE, IPO, PEZA, BSP assistance.",
+		src: 'specialized-registration'
 	},
 	{
 		icon: <FaRocket className="text-4xl text-black"/>,
 		title: "Business Support Services",
 		desc: "Website development and social media marketing to grow your brand.",
+		src: 'business-support'
 	},
 	{
 		icon: <FaPersonWalkingLuggage className="text-4xl text-black"/>,
 		title: "Alien Registration Services",
 		desc: "Assisting foreign nationals with Alien Registration and immigration compliance.",
+		src: 'alien-registration'
 	},
 	{
 		icon: <PiCertificateFill className="text-4xl text-black"/>,
 		title: "Certification Services",
 		desc: "Assisting clients in securing essential business and government certifications.",
+		src: 'certification'
 	},
 	{
 		icon: <FaFlag className="text-4xl text-black"/>,
 		title: "Citizenship Services",
 		desc: "Guiding you through every step of your Philippine citizenship journey.",
+		src: 'citizenship'
 	},
 	{
 		icon: <FaFileSignature className="text-4xl text-black"/>,
 		title: "Special Permit Services",
 		desc: "Making it easy to apply for and obtain special permits.",
+		src: 'special-permits'
 	},
 ];
 
@@ -120,13 +132,16 @@ const OurExpertiseSection = () => {
 							whileInView={{opacity: 1, y: 0}}
 							transition={{duration: 0.4, delay: index * 0.1}}
 							viewport={{once: true}}
-							className="p-6 text-center transition-all duration-300 bg-white border border-gray-200 shadow-sm hover:shadow-lg rounded-2xl hover:-translate-y-1"
+							className="text-center transition-all duration-300 bg-white border border-gray-200 shadow-sm hover:shadow-lg rounded-2xl hover:-translate-y-1"
 						>
-							<div className="flex justify-center mb-4">{service.icon}</div>
-							<h4 className="text-lg font-bold text-[#004524] mb-2">
-								{service.title}
-							</h4>
-							<p className="text-sm text-gray-600">{service.desc}</p>
+							<Link to={`services/${service.src}`} target="_blank"
+								  className="block p-6">
+								<div className="flex justify-center mb-4">{service.icon}</div>
+								<h4 className="text-lg font-bold text-[#004524] mb-2">
+									{service.title}
+								</h4>
+								<p className="text-sm text-gray-600">{service.desc}</p>
+							</Link>
 						</motion.div>
 					))}
 				</div>
